d6a1be4f43e2ca7fff02a36b3230ad3ea6f7ee76
tonilogar <tologar@gmail.com>
2015-03-26 23:32:03 +0100

    Creada clase archivoproyecto

--- ProyectoTP/archivoproyecto.cpp
+++ ProyectoTP/archivoproyecto.cpp
@@ -1,6 +1,125 @@
+//    ToolsPcot
+//    Copyright (C) {2014}  {Antonio López García}
+//    tologar@gmail.com
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "archivoproyecto.h"
 
 
-ArchivoProyecto::ArchivoProyecto()
+ArchivoProyecto::ArchivoProyecto(QObject *parent, QString nameFileProyect):
+    QObject(parent)
 {
+    _nameFileProyect=nameFileProyect;
 }
+bool ArchivoProyecto::exist()
+{
+    return QFileInfo::exists(_nameFileProyect);
+}
+QString ArchivoProyecto::getnameFileProyect()
+{
+    return _nameFileProyect;
+}
+
+QString ArchivoProyecto::getnameProyect()
+{
+    return _nameProyect;
+}
+QString ArchivoProyecto::getdescriptionProyecte()
+{
+    return _descriptionProyecte;
+}
+QString ArchivoProyecto::getautorProyect()
+{
+    return _autorProyect;
+}
+QDate ArchivoProyecto::getdateAcces()
+{
+    return _dateAcces;
+}
+QDate ArchivoProyecto::getdateCreate()
+{
+    return _dateAcces;
+}
+QString ArchivoProyecto::getnumberVersion()
+{
+    return QString("0.0");
+}
+
+void ArchivoProyecto::setnameFileProyect(QString nameFileProyect)
+{
+    _nameFileProyect=nameFileProyect;
+}
+void ArchivoProyecto::setnameProyect(QString nameProyect)
+{
+    _nameProyect=nameProyect;
+}
+void ArchivoProyecto::setdescriptionProyecte(QString descriptionProyecte)
+{
+    _descriptionProyecte=descriptionProyecte;
+}
+void ArchivoProyecto::setautorProyect(QString autorProyect)
+{
+    _autorProyect=autorProyect;
+}
+void ArchivoProyecto::setdateAcces(QDate dateAcces)
+{
+    _dateAcces=dateAcces;
+}
+void ArchivoProyecto::setdateCreate(QDate dateCreate)
+{
+    _dateCreate=dateCreate;
+}
+bool ArchivoProyecto::build(QString nameProyect,QString descriptionProyecte, QString autorProyect)
+{
+    QDate dateCreate=QDate::currentDate();
+
+    if (nameProyect.isEmpty() || nameProyect.isNull())
+    {
+        return false;
+    }
+    QJsonObject proyecto;
+    proyecto.insert("cabecera","PROYECTOTPC");
+    proyecto.insert("version",getnumberVersion());
+    proyecto.insert("proyecto",nameProyect);
+    proyecto.insert("autor",autorProyect);
+    proyecto.insert("descripcion",descriptionProyecte);
+    proyecto.insert("fechacreacion",dateCreate.toString("dd-MM-yyyy"));
+    proyecto.insert("fechaultimoacceso",dateCreate.toString("dd-MM-yyyy"));
+    QJsonDocument documentProyecto;
+    documentProyecto.setObject(proyecto);
+    QFile ficheroProyecto(_nameFileProyect);
+    if(!ficheroProyecto.open(QFile::Text | QFile::WriteOnly))
+    {
+        return false;
+    }
+    ficheroProyecto.write(documentProyecto.toJson());
+    ficheroProyecto.close();
+    _nameProyect=nameProyect;
+    _autorProyect=autorProyect;
+    _descriptionProyecte=descriptionProyecte;
+    _dateCreate=dateCreate;
+    _dateAcces=dateCreate;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- ProyectoTP/archivoproyecto.h
+++ ProyectoTP/archivoproyecto.h
@@ -2,12 +2,51 @@
 #define ARCHIVOPROYECTO_H
 
 #include "proyectotp_global.h"
+#include <QObject>
+#include <QFileInfo>
+#include <QFile>
+#include <QDate>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonValue>
 
-class PROYECTOTPSHARED_EXPORT ArchivoProyecto
+class PROYECTOTPSHARED_EXPORT ArchivoProyecto:public QObject
 {
-
+    Q_OBJECT
 public:
-    ArchivoProyecto();
+    explicit ArchivoProyecto(QObject *parent = 0, QString nameFileProyect=QString());
+    QString getnameFileProyect();
+    QString getnameProyect();
+    QString getdescriptionProyecte();
+    QString getautorProyect();
+    QDate   getdateAcces();
+    QDate   getdateCreate();
+    virtual QString getnumberVersion();
+    bool exist();
+    virtual bool build(QString nameProyect,QString descriptionProyecte, QString autorProyect);
+
+public slots:
+    void setnameFileProyect(QString nameFileProyect);
+    void setnameProyect(QString nameProyect);
+    void setdescriptionProyecte(QString descriptionProyecte);
+    void setautorProyect(QString autorProyect);
+    void setdateAcces(QDate dateAcces);
+    void setdateCreate(QDate dateCreate);
+protected:
+    QString _nameFileProyect;
+    QString _nameProyect;
+    QString _descriptionProyecte;
+    QString _autorProyect;
+    QDate _dateAcces;
+    QDate _dateCreate;
+
+
+
+
+
+
 };
+
 
 #endif // ARCHIVOPROYECTO_H
