b826f1ad8e6bd09dc6b66575a34ca44789b53d19
tonilogar <tologar@gmail.com>
2015-06-29 22:54:20 +0200

    Editando metodos de la clase sectioncoordinates.cpp

--- CoordinatesTP/sectioncoordinates.cpp
+++ CoordinatesTP/sectioncoordinates.cpp
@@ -20,6 +20,9 @@ QString SectionCoordinates::getNombreSection() const
 
 bool SectionCoordinates::processSection(QJsonObject archivo)
 {
+    if(!_modeloCoordenadas)
+        return false;
+
     if(!archivo.contains(getNombreSection()))
         return false;
 
@@ -27,25 +30,39 @@ bool SectionCoordinates::processSection(QJsonObject archivo)
 
     if(!coordenadas.contains("listado"))
         return false;
-
     QJsonArray listaCoordenadas=coordenadas.value("listado").toArray();
-    QJsonArray::iterator it;
-    for(it=listaCoordenadas.begin();it!=listaCoordenadas.end();it++) {
-        QJsonObject identificadorCoordenadas=(*it).toObject();
-        if(identificadorCoordenadas.contains("Id"))
-            qDebug() <<"Id: " <<identificadorCoordenadas.value("Id").toString();
-        if(identificadorCoordenadas.contains("Xa"))
-            qDebug() <<"Xa: "<<identificadorCoordenadas.value("Xa").toDouble();
-        if(identificadorCoordenadas.contains("Xb"))
-            qDebug() << "Xb: "<<identificadorCoordenadas.value("Xb").toDouble();
-        if(identificadorCoordenadas.contains("Ya"))
-            qDebug() << "Ya: "<<identificadorCoordenadas.value("Ya").toDouble();
-        if(identificadorCoordenadas.contains("Yb"))
-            qDebug() << "Yb: "<<identificadorCoordenadas.value("Yb").toDouble();
-        qDebug() << "----------------------------------";
+    if(listaCoordenadas.empty())
+    {
+        this->resetSection();
+        return true;
     }
+    QJsonArray::iterator it;
+    QList <IdentificadorCoordenadas *> listaIdCoor;
+    IdentificadorCoordenadas *idecor;
+    for(it=listaCoordenadas.begin();it!=listaCoordenadas.end();it++)
+    {
+        QJsonObject identificadorCoordenadas=(*it).toObject();
+        idecor=new IdentificadorCoordenadas(this);
+
+        if(identificadorCoordenadas.contains("Id"))
+            idecor->setIdentificador(identificadorCoordenadas.value("Id").toString());
+        if(identificadorCoordenadas.contains("Xa"))
+            idecor->setXa(identificadorCoordenadas.value("Xa").toDouble());
+        if(identificadorCoordenadas.contains("Xb"))
+            idecor->setXb(identificadorCoordenadas.value("Xb").toDouble());
+        if(identificadorCoordenadas.contains("Ya"))
+            idecor->setYa(identificadorCoordenadas.value("Ya").toDouble());
+        if(identificadorCoordenadas.contains("Yb"))
+            idecor->setYb(identificadorCoordenadas.value("Yb").toDouble());
+        listaIdCoor.append(idecor);
+    }
+    this->disconnect(_modeloCoordenadas);
+    _modeloCoordenadas->clear();
+    _modeloCoordenadas->setListaRegistro(listaIdCoor);
+    connect(_modeloCoordenadas,SIGNAL(modelReset()),this,SLOT(modeloResetado()));
     _stateChanged=true;
     emit this->estaActualizado(_stateChanged);
+    return true;
 }
 
 QJsonObject SectionCoordinates::writeSection()
